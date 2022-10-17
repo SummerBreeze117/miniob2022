fe69c10970ef0abe18e39f4b1c582794ca4fdd17
co2ma <co2maker@outlook.com>
2022-10-17 20:06:53 +0800

    实现update功能

--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -675,6 +675,26 @@ RC Table::update_record(Trx *trx, const char *attribute_name, const Value *value
   return RC::GENERIC_ERROR;
 }
 
+RC Table::update_record(Trx *trx, Record *record, const FieldMeta *field, const Value value)
+{
+  RC rc = RC::SUCCESS;
+  if (trx) {
+
+  } else {
+    size_t copy_len = field->len();
+    if (field->type() == CHARS) {
+      const size_t data_len = strlen((const char *)value.data);
+      if (copy_len > data_len) {
+        copy_len = data_len + 1;
+      }
+    }
+    // ?????????????
+    memcpy(record->data() + field->offset(), value.data, copy_len);
+    rc = record_handler_->update_record(record);
+  }
+  return rc;
+}
+
 class RecordDeleter {
 public:
   RecordDeleter(Table &table, Trx *trx) : table_(table), trx_(trx)
